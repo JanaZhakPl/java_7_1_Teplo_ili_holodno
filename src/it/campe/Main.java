6145539c5d4d7f59b8f0382ebeeb664857369382
Ksenx73 <hello@itcampe.com>
2021-10-19 11:37:44 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,10 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+        //напишите тут ваш код
+    }
+}
